10f51f85559f6f9512a9ed4dff5337aedb6eea0e
MrSn001 <shaheen36.saa@gmail.com>
2026-05-18 15:10:10 +0400

    Added the Variables for the Hotel Management System

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Variables
+
+            // Guest Information
+            string guestName = "";
+            string guestPhone = "";
+            int loyaltyPoints = 0;
+
+            // Room Information
+            int roomNumber = 0;
+            string roomType = "";
+            double nightlyRate = 0; 
+            string roomNotes = "";
+
+            // Stay Details
+            DateTime checkInDate = DateTime.Now;   
+            DateTime checkOutDate = DateTime.Now;
+            int numberOfNights = 0;
+            double discountPercentage = 0.0;
+
+            // Status Flags
+            bool isRegistered = false;
+            bool isCheckedIn = false;
+
+
+            //////////////////////////////////////////////////////////////////
+            
+            //Process
+
+
+
+
+
+
+
+
         }
     }
 }
