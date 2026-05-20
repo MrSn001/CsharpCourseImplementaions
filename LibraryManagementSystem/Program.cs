ada4df2c1fb4f98d2f351364d5d34636fa2051d7
MrSn001 <shaheen36.saa@gmail.com>
2026-05-20 09:15:35 +0400

    Implemented the switch case setup

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LibraryManagementSystem
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LibraryManagementSystem
 {
     internal class Program
     {
@@ -37,7 +39,65 @@
                 option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
+                    //Register Member
+                    case 0:
+                        break;
 
+                    //Display Member Profile
+                    case 1:
+                        break;
+
+                    //Search Book by Title
+                    case 2:
+                        break;
+
+                    //Borrow a Book
+                    case 3:
+                        break;
+
+                    //Return a Book
+                    case 4:
+                        break;
+
+                    //Calculate Late Fine
+                    case 5:
+                        break;
+
+                    //Apply Member Discount
+                    case 6:
+                        break;
+
+                    //Check Borrowing Eligibility
+                    case 7:
+                        break;
+
+                    //Register Book 
+                    case 8:
+                        break;
+
+                    //Generate Member ID 
+                    case 9:
+                        break;
+
+                    //Display Book Details 
+                    case 10:
+                        break;
+
+                    //Calculate Renewal Fee
+                    case 11:
+                        break;
+
+                    //Update Member Email
+                    case 12:
+                        break;
+
+                    //Session Summary
+                    case 13:
+                        break;
+                    //temporary
+                    case 14:
+                        loopFlag = false;
+                        break;
                 }
 
 
