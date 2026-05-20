d27fd35e2a4ffe23dcb7071161baa0bcff9696b4
MrSn001 <shaheen36.saa@gmail.com>
2026-05-20 09:07:14 +0400

    Added the storage variables

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -2,9 +2,49 @@
 {
     internal class Program
     {
+        //Member Variables Storage:
+        static string memberName;
+        static int memberID;
+        static string memberEmail;
+        static string membershipExpireDate;
+        static string memberTier;
+        static bool memberIsRegistered;
+
+
+        //Library Variables Storage:
+        static string bookTitle;
+        static string bookAuthor;
+        static string bookGenre;
+        static int numberOfAvailableCopies;
+        static bool bookIsRegistered;
+
+
+        //Library session Variables:
+        static int totalBookBorrowed;
+        static double totalFines;
+
+        //flags Variables
+        static bool loopFlag = true;
+
+        //Choice Variables
+        static int option;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (loopFlag)
+            {
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+
+                }
+
+
+                Console.WriteLine("Enter any key to continue....");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 }
