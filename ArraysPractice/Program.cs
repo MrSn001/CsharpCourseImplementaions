050fb15a4ca38d05ff5790128fcfb04dfb4b7dbd
MrSn001 <shaheen36.saa@gmail.com>
2026-06-02 08:36:52 +0400

    Created the MainMenu Method to be print

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        static void MainMenu()
+        {
+            Console.WriteLine("""
+                1. Temperature Log
+                2. Student Score Board
+                3. Product Price Finder
+                4. Race Finish Times
+                5. Classroom grade Report
+                6. Warehouse Inventory Check
+                7. Library Book Shelf Scanner
+                8. Sales Performance Analyzer
+                9. Flight Seat Allocation Display
+                10. Hospital Patient Priority Queue
+                11. Exit
+                """);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
