f9cc7ec887df57f8ece8818c90b0362df831aeef
MrSn001 <shaheen36.saa@gmail.com>
2026-06-04 08:52:34 +0400

    Implemented the switch setup for the project

--- StackQueuePracticeTask/Program.cs
+++ StackQueuePracticeTask/Program.cs
@@ -2,9 +2,81 @@
 {
     internal class Program
     {
+        //Variables Decleration
+        static int choice;
+        static bool flag;
+
+
+        //Stack Decleration
+
+
+        //Queue Decleration
+
+
+
+        //Method Decleration
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (flag) 
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    //Browser History Tracker 
+                    case 1:
+                        break;
+
+                    //Hotel Check-In Queue
+                    case 2:
+                        break;
+
+                    //Text Editor Undo System
+                    case 3:
+                        break;
+
+                    //Hospital Emergency Room Triage
+                    case 4:
+                        break;
+
+                    //Parenthesis Validator
+                    case 5:
+                        break;
+
+                    //Print Spooler with Priority Re-Insertion
+                    case 6:
+                        break;
+
+                    //Reverse a Sentence Word by Word
+                    case 7:
+                        break;
+
+                    //Multi-Level Undo with Redo
+                    case 8:
+                        break;
+
+                    //Ticket Counter Simulation
+                    case 9:
+                        break;
+
+                    //Order Processing Pipeline with Statistics
+                    case 10:
+                        break;
+
+
+                    case 11:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option!!");
+                        break;
+                
+                }
+            }
         }
     }
 }
